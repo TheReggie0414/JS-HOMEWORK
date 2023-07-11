0f3477eed1ee3917876c354b7b5e764b84b1cf72
TheReggie0414 <kisilvlad0414@gmail.com>
2023-07-11 13:58:08 +0300

    домашка 4

--- script.js
+++ script.js
@@ -1,107 +1,215 @@
-/* String: greeting */
-
-/* let askName = prompt('Как вас зовут?: ');
-
-alert('Привіт ' + askName); */
+/* Number: odd */
 
 
 
-/////////////////////////////////////////////////
+/* const userInput = prompt("Введіть число:");
+
+const number = parseInt(userInput);
 
 
-/* String: gopni4ek */
-
-/* let str = prompt("Пожалуйста вставте рядок: ");
-
-let modString = str.replace(/,/g, ", блин");
-
-console.log(modString); */
-
-
-////////////////////////////////////////////////
-
-
-/* String: capitalize */
-
-/* let str = "cANBerRa";
-
-let result = str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
-
-console.log(result); */
+if (isNaN(number)) {
+  console.log("Помилка! Некоректне число.");
+} else {
+  if (number % 2 === 0) {
+    alert("Введене число є парним.");
+  } else {
+    alert("Введене число є непарним.");
+  }
+} */
 
 
-////////////////////////////////////////////////
+/* Boolean */
+
+/* 
+const answer1 = confirm("Чи володієте ви водійським посвідченням?");
+const answer2 = confirm("Як ви ставитесь до електромобілів?");
+
+console.log("Відповідь на перше питання:", answer1);
+console.log("Відповідь на друге питання:", answer2);
+
+ */
 
 
-/* String: word count */
-
-/* let str = 'Посчитайте количество слов в этом рядке';
-
-let words = str.split(" ");
-
-let wordCount = words.length;
-
-console.log(wordCount); */
+/* Boolean: if */
 
 
-//////////////////////////////////////////////////
+/* const answer = confirm("Яка ваша стать?");
+
+console.log("Відповідь на питання:", answer);
+
+if (answer) {
+  alert("Ви жінка");
+} else {
+  alert("Ви чоловік");
+} */
 
 
-/* String: credentials */
+/* Ternary */
 
-/* let lastName = prompt('Введите вашу фамилию: ');
-let firstName = prompt('Введите ваше имя: ');
-let middleName = prompt('Введите ваше отчество: ');
+/* 
+const ansMale = confirm("Ви чоловік?");
 
-lastName = lastName.trim();
-firstName = firstName.trim();
-middleName = middleName.trim();
-
-firstName = firstName.charAt(0).toUpperCase() + firstName.slice(1).toLowerCase();
-lastName = lastName.charAt(0).toUpperCase() + lastName.slice(1).toLowerCase();
-middleName = middleName.charAt(0).toUpperCase() + middleName.slice(1).toLowerCase();
+const message = ansMale ? "Ви чоловік" : "Ви жінка";
+alert(message); */
 
 
-let fullName = lastName + " " + firstName + " " + middleName;
+/* // bool type cast
+!!2 - true
+!!0 - false
+!!1 - true
+// or
+2 || 1 - 2
+2 || 0 - 2
+//and
+2 && 1 - 1
+1 && 2 - 2
+0 && 2 - 0
+// or and and difference
+0 || 1 || 2 - 1
+0 && 1 && 2 - 0 
+2 || 1 || 0 - 2
+2 && 1 && 0 - 0
+confirm('left') || confirm('right')
+confirm('left') && confirm('right')
+//null, undefined, so on
+null || 2 - 2
+undefined && 1 - undefined
+alert("Hello") && confirm('Are you sexy?');
+alert("Hello") || confirm('Are you drunk?');
+//brackets and complex expressions
+(undefined || 2) && (3 || 0) - 2 && 3 - 3
+(2 && 1) || (null && 0) - 1 || null - 1
+(2 > 1) && "greater" - 2 && "greater" - "greater"
+(2 < 1) && null - false && null - false
+// ternary operator
+1 ? "one" : "not one" - "one"
+0 ? "zero" : "not zero" - "not zero"
+"0" ? "\"zero\"" : "not `zero`" - "\zero\""
+parseInt("0") ? 'true' : 'false' - 'false'
+("" || 2) && (3 || "3.5") || (4 && 5) - (2 && 3 || 5)  (3 || 5)  (3)
+(-1 + 1) && "zero" - 0 && "zero" - 0
+"-1" + 1 && "oups" - -11 && "oups" - "oups"
+(typeof null === 'object') ? "null is object" : "null is null" - true ? "null is object" : "null is null" - "null is object"
+// ternary && ||
+Math.random() < 0.5 && 'less' || 'more' - false && 'less' || 'more' - false || 'more' - 'more' (шанс 50% так як Math.random це число від 0 до 1)
+(a = Math.random()) < 0.5 && 'less: '+a || 'more: '+a - false && 'less: '+a || 'more: '+a  Тут неможу зрозуміти
+//in for array
+[2,3,5,7,11].indexOf(7) > -1 ? 'prime' : 'not found' - true ? 'prime': 'not found' - 'prime' */
 
-console.log(fullName); */
+
+/* Prompt: or */
+
+/* 
+let age = prompt("Введіть ваш вік:");
+
+if (!age) {
+  alert("Помилка! Вік не введений або скасовано.");
+} else {
+  alert("Ваш вік: " + age);
+}
+ */
 
 
-///////////////////////////////////////////////////////
+
+/* Confirm: or this days */
+
+/* confirm("Шопінг?") || alert("Ти - бяка"); */
 
 
-/* String: beer */
+/* 
+Confirm: if this days */
 
-/* let str = "Було жарко. Василь пив пиво вприкуску з креветками";
-let result = str.split("пиво").join("чай");
+/* let shopping = confirm("Шопінг?");
 
-console.log(result); */
-
-
-///////////////////////////////////////////////////////
-
-
-/* String: no tag */
+if (!shopping) {
+  alert("Ти - бяка");
+} */
 
 
-/* let str = "якийсь текст, в якому є один тег <br /> і всяке інше";
-let firstIndex = str.indexOf("<");
-let closeIndex = str.indexOf(">");
-
-let result = str.slice(0, firstIndex) + str.slice(closeIndex + 1);
-
-console.log(result); */
 
 
-////////////////////////////////////////////////////////
+/* Default: or */
 
 
-/* String: big tag */
 
-/* let str = "якийсь текст у якому є один тег <br /> і всяке інше";
-let tag = "<br />";
-let capitalizedTag = tag.toUpperCase();
+/* let firstName = prompt("Введіть ім'я:") || "Іван";
+let lastName = prompt("Введіть прізвище:") || "Іванов";
+let fullName = prompt("Введіть повне ім'я:") || (firstName + " " + lastName);
 
-let result = str.split(tag).join(capitalizedTag);
+console.log("Ім'я: " + firstName);
+console.log("Прізвище: " + lastName);
+console.log("Повне ім'я: " + fullName); */
 
-console.log(result); */
+
+/* Default: if */
+
+
+/* let firstName = prompt("Введіть ім'я:");
+if (!firstName) {
+  firstName = "Іван";
+}
+
+let lastName = prompt("Введіть прізвище:");
+if (!lastName) {
+  lastName = "Іванов";
+}
+
+let fullName;
+if (!fullName) {
+  fullName = firstName + " " + lastName;
+}
+
+console.log("Ім'я: " + firstName);
+console.log("Прізвище: " + lastName);
+console.log("Повне ім'я: " + fullName); */
+
+
+
+
+/* Login and password */
+
+/* let login = prompt("Введіть логін:");
+
+if (login === "admin") {
+  let password = prompt("Введіть пароль:");
+  
+  if (password === "qwerty") {
+    alert("Логін успішний! Вітаємо, admin!");
+  } else {
+    alert("Помилка! Невірний пароль.");
+  }
+} else {
+  alert("Помилка! Невірний логін.");
+} */
+
+
+/* Scissors */
+
+/* let userChoice = prompt("Введіть свій варіант: камінь, ножиці або папір").toLowerCase();
+
+let computerChoice = Math.random();
+if (computerChoice < 0.33) {
+  computerChoice = "камінь";
+} else if (computerChoice < 0.66) {
+  computerChoice = "ножиці";
+} else {
+  computerChoice = "папір";
+}
+
+alert("Варіант програми: " + computerChoice);
+
+if (userChoice === computerChoice) {
+  alert("Нічия!");
+} else if (
+  (userChoice === "камінь" && computerChoice === "ножиці") ||
+  (userChoice === "ножиці" && computerChoice === "папір") ||
+  (userChoice === "папір" && computerChoice === "камінь")
+) {
+  alert("Ви перемогли!");
+} else {
+  alert("Комп'ютер переміг!");
+} */
+
+
+
+
