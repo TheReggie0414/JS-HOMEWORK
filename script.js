05eb3873a3efa98fa7199da04134e31793440cd8
Vladyslav Kysil <109174554+TheReggie0414@users.noreply.github.com>
2023-09-01 16:54:11 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,65 +1,28 @@
-const measureBucket = () => {
-    let bucketOne = 5;
-    let bucketTwo = 3;
+const squares = document.querySelectorAll('.square');
+const btnSend = document.getElementById('btnSend');
 
-    return function() {
-        bucketOne -= 1;
-        bucketTwo += 1;
-        return ("В первом ведре: " + bucketOne + " л, а во втором ведре: " + bucketTwo + " л")
-    };
-};
+for (let i of squares) {
+    i.addEventListener('click', function () {
+        i.classList.toggle('cat');
+    })
+}
 
-const measure = measureBucket();
-const finalMeasure = measure();
-console.log(finalMeasure); 
+class Square {
+    constructor(element) {
+        this.element = element;
+        this.selected = false;
+        this.element.addEventListener('click', () => this.clickSelection());
+    }
 
+    clickSelection() {
+        this.selected = !this.selected;
+    }
+}
 
+const allSquareObj = [];
+squares.forEach(item => allSquareObj.push(new Square(item)));
 
-
-
-
-// function getRandomInt(min, max) {
-//     return Math.floor(Math.random() * (max - min + 1)) + min;
-//   }
-  
-
-//   const roundsToWin = 3;
-  
-
-//   let teamAScore = 0;
-//   let teamBScore = 0;
-//   let currentRound = 1;
-  
-//   while (teamAScore < roundsToWin && teamBScore < roundsToWin) {
-//     console.log(`Раунд ${currentRound}`);
-    
-
-//     const zones = [1, 2, 3, 4];
-//     for (let i = zones.length - 1; i > 0; i--) {
-//       const j = getRandomInt(0, i);
-//       [zones[i], zones[j]] = [zones[j], zones[i]];
-//     }
-    
-
-//     const winningZone = zones[getRandomInt(0, 2)];
-//     console.log(`Победившая зона: ${winningZone}`);
-    
-
-//     if (winningZone === 4) {
-//       teamBScore++;
-//       console.log("Команда B получает очко!");
-//     } else {
-//       teamAScore++;
-//       console.log("Команда A получает очко!");
-//     }
-    
-//     console.log(`Счет: Команда A - ${teamAScore}, Команда B - ${teamBScore}\n`);
-//     currentRound++;
-//   }
-  
-
-//   if (teamAScore >= roundsToWin) {
-//     console.log("Команда A выигрывает матч!");
-//   } else {
-//     console.log("Команда B выигрывает матч!");
-//   }
+btnSend.addEventListener('click', () => {
+    const selectedSquares = allSquareObj.filter(square => square.selected);
+    console.log(selectedSquares);
+});
